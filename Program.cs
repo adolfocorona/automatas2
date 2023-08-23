6917eb08fe9383312e713c0c5c3f22d7a1b66082
unknown <adolfcorona@gmail.com>
2023-08-23 13:47:18 -0600

    New files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sintaxis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Token T = new Token();
+                T.setContenido("3.5e-8");
+                T.setClasificacion(Token.Tipos.Numero);
+                Console.WriteLine(T.getContenido());
+                Console.WriteLine(T.getClasificacion());
+                /*using (Lenguaje L = new Lenguaje("suma.cpp"))
+                {
+                    //L.Programa();
+                    
+                    while (!L.FinArchivo())
+                    {
+                        L.nextToken();
+                    }
+                    
+                }*/
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
